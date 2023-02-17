46f9d1f6144d1ce20f50bb578855eaa8f2b9a0a6
chaseknis <chase6th@gmail.com>
2023-02-17 11:00:55 +0200

    Create logic directory Create operate.js and calculate.js files install big.js Implement click event handlers Run linters for errors Update README.md file

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,34 +1,54 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import calculate from '../logic/calculate';
 
 export default function Calculator() {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
+
+  const onClickHandler = useCallback((e) => {
+    const results = calculate(state, e.target.innerText);
+    setState(results);
+  }, [state]);
+
+  const { total, next, operation } = state;
+
   return (
     <div className="calculator_container">
-      <p>0</p>
+      <p>
+        {total}
+        {' '}
+        {operation}
+        {' '}
+        {next}
+      </p>
       <div className="number_wrapper">
         <div className="row_container">
-          <button type="button">AC</button>
-          <button type="button">+/-</button>
-          <button type="button">%</button>
-          <button type="button" className="orange">/</button>
+          <button type="button" onClick={onClickHandler}>AC</button>
+          <button type="button" onClick={onClickHandler}>+/-</button>
+          <button type="button" onClick={onClickHandler}>%</button>
+          <button type="button" onClick={onClickHandler} className="orange">/</button>
 
-          <button type="button">9</button>
-          <button type="button">8</button>
-          <button type="button">7</button>
-          <button type="button" className="orange">x</button>
+          <button type="button" onClick={onClickHandler}>9</button>
+          <button type="button" onClick={onClickHandler}>8</button>
+          <button type="button" onClick={onClickHandler}>7</button>
+          <button type="button" onClick={onClickHandler} className="orange">x</button>
 
-          <button type="button">6</button>
-          <button type="button">5</button>
-          <button type="button">4</button>
-          <button type="button" className="orange">-</button>
+          <button type="button" onClick={onClickHandler}>6</button>
+          <button type="button" onClick={onClickHandler}>5</button>
+          <button type="button" onClick={onClickHandler}>4</button>
+          <button type="button" onClick={onClickHandler} className="orange">-</button>
 
-          <button type="button">3</button>
-          <button type="button">2</button>
-          <button type="button">1</button>
-          <button type="button" className="orange">+</button>
+          <button type="button" onClick={onClickHandler}>3</button>
+          <button type="button" onClick={onClickHandler}>2</button>
+          <button type="button" onClick={onClickHandler}>1</button>
+          <button type="button" onClick={onClickHandler} className="orange">+</button>
 
-          <button type="button" className="zero">0</button>
-          <button type="button">.</button>
-          <button type="button" className="orange">=</button>
+          <button type="button" onClick={onClickHandler} className="zero">0</button>
+          <button type="button" onClick={onClickHandler}>.</button>
+          <button type="button" onClick={onClickHandler} className="orange">=</button>
         </div>
       </div>
     </div>
